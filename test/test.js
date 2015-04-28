83ebe61ed7bd6da116a81fa08d8a9f65b8a123eb
Neil O'Toole <neilotoole@apache.org>
2015-04-28 15:10:03 +0100

    adding IDE and other setup files

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+var should = require("should");
+
+
+describe('trivial test', function() {
+	it('will all be ok', function() {
+		(5).should.be.exactly(5);
+	});
+});
